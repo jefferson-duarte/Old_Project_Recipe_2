9802e4f48b824c81ccebed4f2c86fe6230e0a897
Jefferson Duarte <jeffersonsduarte@live.com>
2022-08-11 13:25:17 +0100

    Configured css

--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'recipes/pages/home.html')
