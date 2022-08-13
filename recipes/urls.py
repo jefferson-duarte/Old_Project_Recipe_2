d2771a182218ea9c62f7e426c2792b4a76cae4b5
Jefferson Duarte <jeffersonsduarte@live.com>
2022-08-13 10:58:16 +0100

    Fixed bugs urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('recipes/<int:id>/', views.recipe, name='recipe'),
